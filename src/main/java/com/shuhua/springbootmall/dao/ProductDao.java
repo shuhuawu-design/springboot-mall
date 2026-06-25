dbed1e491e24d77a2b30c20fba7ff1f052594c81
shuhuawu <justina50089@gmail.com>
2026-06-25 10:03:19 +0800

    商品功能-實作查詢商品列表(分頁 + 總筆數)

--- src/main/java/com/shuhua/springbootmall/dao/ProductDao.java
+++ src/main/java/com/shuhua/springbootmall/dao/ProductDao.java
@@ -7,6 +7,7 @@ import com.shuhua.springbootmall.model.Product;
 import java.util.List;
 
 public interface ProductDao {
+    Integer countProduct(ProductQueryParams productQueryParams);
     Product getProductById(Integer productId);
     Integer createProduct(ProductRequest productRequest);
     void updateProduct(Integer productId, ProductRequest productRequest);
